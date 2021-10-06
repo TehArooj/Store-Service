27b532e041528ade2597723f8675e359497b09fa
TehArooj <58910327+TehArooj@users.noreply.github.com>
2021-10-06 20:14:35 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+/*function sayHello(name){
+    console.log('Hello '+name);
+}
+
+//sayHello('Tehreem');
+console.log(window);
+
+const log = require('./logger');
+
+log('message');
+
+//console.log(logger);
+
+const path = require('path'); 
+
+var pathObject=path.parse(__filename);
+
+console.log(pathObject);*/
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors=require('cors');
+require('dotenv/config');
+
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const databaseName = 'test'
+const client = new MongoClient(url);
+
+async function getData() {
+    let result = await client.connect();
+    db = result.db(databaseName);
+    collection = db.collection('posts')
+    console.log(`****DB connected **** ${db}`);
+    let data = await collection.find({}).toArray();
+    console.log(data)
+}
+
+getData();
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+//Import Routes
+
+const postRoutes = require('./routes/posts');
+
+
+//Middlewares
+
+app.use('/posts', postRoutes);
+
+//ROUTES
+
+app.get('/', (req, res) => {
+    res.send(' Welcome! We are in home');
+});
+
+//CONNECT TO DB
+
+mongoose.connect(process.env.DB_CONNECTION)
+    .then(() => console.log("Connected to DB....."))
+    .catch(err => console.log("Failed to connect to DB", err));
+
+
+//Listening 
+app.listen(3000);
---
+++ logger.js
@@ -0,0 +1,11 @@
+/*var url='http://mylogger.io/log';
+
+function log(message){
+    //Send HTTP request
+    console.log(message);
+}
+
+
+module.exports=log;  //exporting function only 
+module.exports.log=log;  //object 
+//module.exports.url=url;*/
